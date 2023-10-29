6557194603d11aae29d9c33b47e12aaae83f275c
Hieubentau <79564920+Hieubentau@users.noreply.github.com>
2023-10-30 01:56:53 +0700

    create example

---
+++ src/components/RowText.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+const RowText = (props) => {
+  const {
+    messageOne,
+    messageTwo,
+    containerStyles,
+    messageOneStyles,
+    messageTwoStyles
+  } = props
+  return (
+    <View style={containerStyles}>
+      <Text style={messageOneStyles}>{messageOne}</Text>
+      <Text style={messageTwoStyles}>{messageTwo}</Text>
+    </View>
+  )
+}
+
+export default RowText
