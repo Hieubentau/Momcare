c935f33d6b2e9ec545a58ecfb33c3cb89cfc5da0
Hieubentau <79564920+Hieubentau@users.noreply.github.com>
2023-11-08 16:49:43 +0700

    make simple list for doctor speciality

--- src/components/HomeScreen/DoctorSpeciality.js
+++ src/components/HomeScreen/DoctorSpeciality.js
@@ -1,31 +1,74 @@
-import React, { useContext } from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useContext, useState } from 'react'
+import { FlatList, View, Text, TouchableOpacity } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import { ThemeColorContext } from '../../contexts/themeColorContext'
+import { doctorSpecialityType } from '../../ultilities/doctorSpecialityType'
+
+const Item = ({ item, onPress, backgroundColor, styles }) => (
+  <View style={styles.avatarWrapper}>
+    <View style={styles.avatarTouchableWrapper}>
+      <TouchableOpacity
+        onPress={onPress}
+        style={[styles.avatarTouchable, { backgroundColor }]}
+      >
+        <MaterialCommunityIcons
+          name={item.icon}
+          size={24}
+          color={styles.themeColor}
+        />
+      </TouchableOpacity>
+    </View>
+    <Text>{item.message}</Text>
+  </View>
+)
 
 const DoctorSpeciality = (props) => {
-  const { doctorSpecialityText } = props
-  const { avatarTouchableWrapper, avatarTouchable } = styles
+  const [selectedId, setSelectedId] = useState()
 
   const themeColor = useContext(ThemeColorContext)
+
+  const { doctorSpecialityText } = props
+  const { avatarWrapper, avatarTouchableWrapper, avatarTouchable } = styles
+
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? 'silver' : 'gainsboro'
+
+    return (
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+        backgroundColor={backgroundColor}
+        styles={{
+          avatarWrapper,
+          avatarTouchableWrapper,
+          avatarTouchable,
+          themeColor
+        }}
+      />
+    )
+  }
+
   return (
     <View>
       <Text style={doctorSpecialityText}>Doctor Speciality</Text>
-      <View style={avatarTouchableWrapper}>
-        <TouchableOpacity style={avatarTouchable}>
-          <MaterialCommunityIcons
-            name="account-multiple"
-            size={24}
-            color={themeColor}
-          />
-        </TouchableOpacity>
+      <View>
+        <FlatList
+          data={doctorSpecialityType}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          extraData={selectedId}
+        />
       </View>
     </View>
   )
 }
 
 const styles = {
+  avatarWrapper: {
+    width: 48,
+    alignItems: 'center'
+  },
   avatarTouchableWrapper: {},
   avatarTouchable: {
     backgroundColor: 'gainsboro',
---
+++ src/ultilities/doctorSpecialityType.js
@@ -0,0 +1,57 @@
+export const doctorSpecialityType = [
+  {
+    id: 1,
+    icon: 'account-multiple',
+    message: 'General'
+  },
+  {
+    id: 2,
+    icon: 'account-multiple',
+    message: 'Cardiologist'
+  },
+  {
+    id: 3,
+    icon: 'account-multiple',
+    message: 'Immunologist'
+  },
+  {
+    id: 4,
+    icon: 'account-multiple',
+    message: 'Neurologist'
+  },
+  {
+    id: 5,
+    icon: 'account-multiple',
+    message: 'Allergist'
+  },
+  {
+    id: 6,
+    icon: 'account-multiple',
+    message: 'Dentist'
+  },
+  {
+    id: 7,
+    icon: 'account-multiple',
+    message: 'Opthalmologist'
+  },
+  {
+    id: 8,
+    icon: 'account-multiple',
+    message: 'Nutritionist'
+  },
+  {
+    id: 9,
+    icon: 'account-multiple',
+    message: 'Pediatric'
+  },
+  {
+    id: 10,
+    icon: 'account-multiple',
+    message: 'Radiologist'
+  },
+  {
+    id: 11,
+    icon: 'account-multiple',
+    message: 'Psychiatrist'
+  }
+]
